585a6621f22ff16e2f4f2c16db37f2b24b2f39c8
Derrick Emery <derricke@users.noreply.github.com>
2025-08-05 17:16:09 -0500

    Add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+        return [
+          {
+            source: '/resume', // The URL you want to redirect from
+            destination: 'https://drive.google.com/file/d/1uEMAJjRM2XcgCggiL5vOAUrwb1FZKwsW/view?usp=sharing', // The URL you want to redirect to
+            permanent: true, // Set to true for a 308 (permanent) redirect, false for a 307 (temporary)
+          },
+          // Add more redirect objects as needed
+        ];
+      },
   output: 'export',
   images: {
     unoptimized: true,
